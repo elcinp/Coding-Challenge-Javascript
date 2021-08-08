9e9034c37d0cb35f7ac544f700366c64651402d6
elcinp <elcinpars1@gmail.com>
2021-08-08 17:50:59 +0300

    third solution

--- 8_Repeat_str_num_times/index.js
+++ 8_Repeat_str_num_times/index.js
@@ -1,7 +1,22 @@
-function repeatStringNumTimes(str,num) {
+// function repeatStringNumTimes(str,num) {
+//     if(num<0) return ""
+//     let repeatedStr = ""
+//     for (let i=0;i<num;i++) {
+//         repeatedStr += str
+//     }
     
-    for (let i=0;i<num;i++) {
+//     return repeatedStr;
+// }
+// console.log(repeatStringNumTimes("ara",0))
 
-    }
-}
 
+
+// function repeatStringNumTimes(str,num) {
+//     if (num<0) return "";
+//     return str.repeat(num);
+// }
+
+// repeatStringNumTimes("hello",4)
+
+
+function repeatStringNumTimes(str,num)
