850eed01e2ba282c3a8118530798bd147b53dc74
elcinp <elcinpars1@gmail.com>
2021-08-13 23:54:58 +0300

    chunky monkey

--- 10_Chunky_Monkey/index.js
+++ 10_Chunky_Monkey/index.js
@@ -11,3 +11,18 @@ function chunkArrayInGroups(arr,size) {
 
 chunkArrayInGroups(["a","b","c","d"],2);
 
+//with splice method
+
+function chunkArrayInGroups(arr,size) {
+    //Break it up
+    let groups = [];
+    while (arr.length>0) {
+        groups.push(arr.splice(0,size))
+        // arr = arr.slice(size);
+    }
+
+    return groups;
+}
+
+chunkArrayInGroups(["a","b","c","d"],2);
+
