307ea3989d7c54de754ebe0463d188f0938fbe79
elcinp <elcinpars1@gmail.com>
2021-08-15 21:30:52 +0300

    2

--- 12_Mutations/index.js
+++ 12_Mutations/index.js
@@ -2,9 +2,18 @@ function mutation(arr) {
     let firstWord = arr[0];
     let secondWord = arr[1];
 
-    for (let i= 0;i<secondWord;i++) {
-        if (firstWord.indexOf(secondword[i]) === -1) return false
+    // for (let i= 0;i<secondWord;i++) {
+    //     if (firstWord.indexOf(secondword[i]) === -1) return false
+    // }
+
+    for (let letter of secondWord) {
+        if(firstWord.imnde)
     }
+
+    for (let letter of secondWord) {
+        if(!firstWord.includes(letter)) return false;
+    }
+
     return true;
 }
 
