94c7d808dba71201a53f81f49a89ab88c9edb319
elcinp <elcinpars1@gmail.com>
2021-09-17 17:41:28 +0300

    prototype

--- 13_gfh/index.js
+++ 13_gfh/index.js
@@ -1,1 +1,12 @@
-const showInfo = "huggg"
+// function sayHello() {
+//     console.log('hello')
+// }
+// console.log(sayHello.prototype)
+
+var Storm =function() {};
+Storm.prototype.precip = 'rain';
+var WinterStorm =function () {};
+WinterStorm.prototype = new Storm();
+WinterStorm.prototype.precip = 'snow';
+var bob = new WinterStorm();
+console.log(bob.precip)
