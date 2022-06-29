cb21882c717ab00d33b031ead924312872666a51
elcinp <elcinpars1@gmail.com>
2022-06-29 13:21:36 +0300

    solution 2 added

--- 7_Confirm_ending/index.js
+++ 7_Confirm_ending/index.js
@@ -6,3 +6,15 @@ console.log(confirmEnding(str));
 /* str="I'm just a singer"
 console.log(str.substr(7));
 console.log(str.slice(7,10)); */
+
+//Solution without endswith()
+
+// function confirmEnding(str, target) {
+//     let len = target.length
+//     if(str.slice(str.length-len,str.length)==target){
+//       return true 
+//     }else {
+//       return false
+//     }
+  
+//   }
