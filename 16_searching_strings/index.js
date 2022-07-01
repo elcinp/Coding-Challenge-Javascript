e2fc7f209cfc80577b29b5e586cb65d758b2eba8
elcinp <elcinpars1@gmail.com>
2022-07-01 10:32:59 +0300

    using match method

--- 16_searching_strings/index.js
+++ 16_searching_strings/index.js
@@ -1,3 +1,14 @@
 //Bir string içerisindeki  sayıları bulup toplamını bulma
 // Mesela "75hello 8"
 // Output: 83
+
+function searchingStrings(str) {
+    var matches = str.match(/\d+/g)
+    sum = 0
+    for (let i=0;i<matches.length;i++){
+        sum +=parseInt(matches[i])
+    }
+    return sum;
+}
+console.log(searchingStrings("75hello5"));
+// searchingStrings("75hello50")
