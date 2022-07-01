871dc06583fe479586c87643c680fee440db572e
elcinp <elcinpars1@gmail.com>
2022-07-01 11:05:19 +0300

    counted

--- 17_arrays/index.js
+++ 17_arrays/index.js
@@ -4,14 +4,26 @@
 // Mesela: [1,2,3,4]
 // Output:0
 
-// tekrar eden sayılardan set oluşturma 
+// tekrar eden sayılardan set oluşturma
 // const numbers = [1, 2, 3, 2, 4, 5, 5, 6];
 // const unique = Array.from(new Set(numbers));
 // console.log(unique);
 // [ 1, 2, 3, 4, 5, 6 ]
 
-function repeatedNums(arr){
+// The Array.from() method, we used above, converts the Set back to an array. This is required because a Set is not an array. You could also use spread operator if you want for conversion:
 
+function repeatedNums(arr) {
+  const count = {};
+  const result = [];
 
-    return arr
+  arr.forEach((item) => {
+    if (count[item]) {
+      count[item] += 1;
+      return;
+    }
+    count[item] = 1;
+  });
+
+  return count;
 }
+console.log(repeatedNums([1,1,2,3,4,4,4,5]));
