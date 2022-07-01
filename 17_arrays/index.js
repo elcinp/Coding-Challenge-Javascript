121df353f0946ff3291e189a58aaedfa5c8d5313
elcinp <elcinpars1@gmail.com>
2022-07-01 10:53:24 +0300

    convert set to array

--- 17_arrays/index.js
+++ 17_arrays/index.js
@@ -4,8 +4,14 @@
 // Mesela: [1,2,3,4]
 // Output:0
 
+// tekrar eden sayılardan set oluşturma 
+// const numbers = [1, 2, 3, 2, 4, 5, 5, 6];
+// const unique = Array.from(new Set(numbers));
+// console.log(unique);
+// [ 1, 2, 3, 4, 5, 6 ]
+
 function repeatedNums(arr){
 
-    
+
     return arr
 }
